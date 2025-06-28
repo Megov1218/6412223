37d7b471f42b36c4a1a4a9e4f4a1eb4bc8a7af2a
Megov1218 <govindwarmeet1224@gmail.com>
2025-06-28 19:40:21 +0530

    Update Task2_Scenerio3.sql

--- Week2/Task2_Scenerio3.sql
+++ Week2/Task2_Scenerio3.sql
@@ -5,24 +5,23 @@ CREATE OR REPLACE PROCEDURE TransferFunds (
 ) AS
     sourceBalance NUMBER;
 BEGIN
-    -- Get source account balance
+    
     SELECT Balance INTO sourceBalance FROM Accounts WHERE AccountID = sourceAccountID FOR UPDATE;
     
-    -- Check for sufficient funds
+   
     IF sourceBalance >= transferAmount THEN
-        -- Deduct from source account
+        
         UPDATE Accounts
         SET Balance = Balance - transferAmount,
             LastModified = SYSDATE
         WHERE AccountID = sourceAccountID;
         
-        -- Add to target account
         UPDATE Accounts
         SET Balance = Balance + transferAmount,
             LastModified = SYSDATE
         WHERE AccountID = targetAccountID;
         
-        -- Record transactions
+        
         INSERT INTO Transactions (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
         VALUES (Transactions_SEQ.NEXTVAL, sourceAccountID, SYSDATE, transferAmount, 'Withdrawal');
         
