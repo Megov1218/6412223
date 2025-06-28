5c01780d6ae73502a051066c1a5b47e6d44b35b8
Megov1218 <govindwarmeet1224@gmail.com>
2025-06-28 19:38:47 +0530

    Create Task2_Scenerio2.sql

---
+++ Week2/Task2_Scenerio2.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
+    deptName IN VARCHAR2,
+    bonusPercent IN NUMBER
+) AS
+BEGIN
+    UPDATE Employees
+    SET Salary = Salary + (Salary * (bonusPercent / 100))
+    WHERE Department = deptName;
+    
+    COMMIT;
+END;
+/
