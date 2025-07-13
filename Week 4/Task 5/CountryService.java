8f63fcb069714a9dab5bb60f6544c156167df247
Megov1218 <govindwarmeet1224@gmail.com>
2025-07-13 11:14:31 +0530

    Create CountryService.java

---
+++ Week 4/Task 5/CountryService.java
@@ -0,0 +1,42 @@
+package com.cognizant.spring_learn.service;
+
+import com.cognizant.spring_learn.Country;
+import org.springframework.stereotype.Service;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countryList = context.getBean("countryList", List.class);
+
+        return countryList.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null); // or throw custom exception
+    }
+}
+3. CountryController
+ src/main/java/com/cognizant/spring_learn/controller/CountryController.java:
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
